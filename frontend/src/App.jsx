cf431009f7b400c459078d8ad70d7ba638372755
Suman-7z <sumankandisuman@gmail.com>
2026-06-20 22:03:15 +0530

    reDesigning ChatWindow.jsx , css and connecting with backend to get response

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,21 @@ import "./App.css";
 import Sidebar from "./Components/Sidebar.jsx";
 import ChatWindow from "./Components/ChatWindow.jsx";
 import { MyContext } from "./Context/MyContext.jsx";
+import { useState } from "react";
+import { v1 as uuidv1 } from "uuid";
 
 function App() {
-  const providerValues = {};
+  const [prompt, setPrompt] = useState("");
+  const [reply, setReply] = useState(null);
+  const [currThreadId, setCurrThreadId] = useState(uuidv1());
+  const providerValues = {
+    prompt,
+    setPrompt,
+    reply,
+    setReply,
+    currThreadId,
+    setCurrThreadId,
+  };
 
   return (
     <div className="app">
--- frontend/src/Components/ChatWindow.jsx
+++ frontend/src/Components/ChatWindow.jsx
@@ -1,15 +1,58 @@
 import "../Components/ChatWindow.css";
 import Chat from "../Components/Chat.jsx";
+import { MyContext } from "../Context/MyContext.jsx";
+import { useContext } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
 export default function ChatWindow() {
+  const { reply, setReply, prompt, setPrompt, currThreadId, setCurrThreadId } =
+    useContext(MyContext);
+
+  const getReply = async () => {
+    const data = {
+      message: prompt,
+      threadId: currThreadId,
+    };
+    try {
+      const response = await axios.post("http://localhost:8080/api/chat", data);
+      setReply(response.data.reply);
+      setPrompt("");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <>
       <div className="chatWindow">
         <div className="navbar">
+          <span>
+            ChatGPT <i className="fa-solid fa-angle-down"></i>
+          </span>
 
+          <div className="userIconDiv">
+            <span className="userIcon">
+              <i className="fa-solid fa-user"></i>
+            </span>
+          </div>
         </div>
         <Chat></Chat>
         <div className="chatInput">
-            
+          <div className="inputBox">
+            <input
+              type="text"
+              placeholder="Ask anything"
+              value={prompt}
+              onChange={(e) => setPrompt(e.target.value)}
+              onKeyDown={(e) => (e.key === "Enter" ? getReply() : "")}
+            />
+            <div id="submit" onClick={getReply}>
+              <i className="fa-solid fa-paper-plane"></i>
+            </div>
+          </div>
+          <p className="info">
+            ChatGPT can make mistakes. Check important info.
+          </p>
         </div>
       </div>
     </>
